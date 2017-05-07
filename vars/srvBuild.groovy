57a10f17ba6015948af0c0ef40fb0cde724aa3e1
hyteer <hyteer@qq.com>
2017-05-08 01:10:38 +0800

    srvBuild

--- vars/srvBuild.groovy
+++ vars/srvBuild.groovy
@@ -13,29 +13,21 @@ def call(body) {
 
     // now build, based on the configuration provided
     node (config.NODE) {
-
-        //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: srvName]], submoduleCfg: [], userRemoteConfigs: [[url: gitUrl]]])
-        //git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
-        stage ('Preparation') {
+        stage ('Pull SCMs') {
           sh 'rm -rf *'
           echo "==Checkout Ops repo..."
           checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: config.GIT_OPS_URL]]])
+          //sh 'printenv'
+          //sh 'echo "Currdir:${PWD}"'
+          echo "Service Name: ${config.SRV_NAME}"
+          echo "Repos: ${config.REPOS}"
+          //git url: "https://github.com/hyteer/${config.repoName}.git"
+          echo "===Checkout service repo..."
+          checkout([$class: 'GitSCM', branches: [[name: config.BRANCH]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: config.SRC_DIR], [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: config.CREDENTIALS_ID, url: config.GIT_SRV_URL]]])
+          //mail to: "...", subject: "${config.name} plugin build", body: "..."
         }
-
-        //sh 'printenv'
-        //sh 'echo "Currdir:${PWD}"'
-        //sh 'echo "YT-srv:${srvName}"'
-        //echo "MyService: ${config.srvName}"
-        //echo "MyRepo: ${config.repoName}"
-        echo "Service Name: ${config.SRV_NAME}"
-        echo "Repos: ${config.REPOS}"
-        //sh "ls"
-        //git url: "https://github.com/hyteer/${config.repoName}.git"
-        echo "===Checkout service repo..."
-        checkout([$class: 'GitSCM', branches: [[name: config.BRANCH]], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: config.SRC_DIR], [$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: true, reference: '', trackingSubmodules: false]], submoduleCfg: [], userRemoteConfigs: [[credentialsId: config.CREDENTIALS_ID, url: config.GIT_SRV_URL]]])
-        //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: config.srvName]], submoduleCfg: [], userRemoteConfigs: [[url: config.gitSrvUrl]]])
-        //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: config.srvName]], submoduleCfg: [], userRemoteConfigs: [[url: GIT_SRV_URL]]])
-
-        //mail to: "...", subject: "${config.name} plugin build", body: "..."
+        stage ('Build Images') {
+          echo "Build Images"
+        }
     }
 }
