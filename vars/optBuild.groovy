57f9c16e3954c417afb0c57882b4211747f7480e
hyteer <hyteer@qq.com>
2017-05-07 19:14:16 +0800

    optBuild

--- vars/optBuild.groovy
+++ vars/optBuild.groovy
@@ -5,10 +5,11 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
+    def buildNode = 'node-5'
 
 
     // now build, based on the configuration provided
-    node 'node-5'{
+    node ('node-5') {
 
         //checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: srvName]], submoduleCfg: [], userRemoteConfigs: [[url: gitUrl]]])
         //git url: "https://github.com/jenkinsci/${config.name}-plugin.git"
